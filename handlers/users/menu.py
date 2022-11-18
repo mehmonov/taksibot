cee9767d0a1a38f7413ac4ee91f9a50407c05506
Husniddin <uzbekfreedom@gmail.com>
2022-11-18 22:09:37 +0500

    ll
    l

--- handlers/users/menu.py
+++ handlers/users/menu.py
@@ -7,4 +7,4 @@ from loader import dp
 
 @dp.message_handler(text='🚖 Buyurtma')
 async def bot_start(message: types.Message):
-    await message.answer(f"Buyurtma turini tanlang: 👇 \n \n 🚕 Oddiy  -  Minimal summa - 5000 so'm. 1km - 500 so'm.  Nexia, matiz. \n \n  ⚡️ Tezkor - 5 daqiqada kelishi ta'minlanadi. Minimal summa - 500 so'm. \n \n 🛵 Yetkazib berish - Minimal summa - 5000 so'm.  1km - 500 so'm \n \n 🚖 Komfort - Komfort mashina. Konditsioner. Minimal summa - 5000  so'm.  1km - 500 so'm \n \n Barcha tarifda minimal yo'nalish 3 km.",reply_markup=buyurtma)
+    await message.answer(f"Bu yurtma turini tanlang: 👇 \n \n 🚕 Oddiy  -  Minimal summa - 5000 so'm. 1km - 500 so'm.  Nexia, matiz. \n \n  ⚡️ Tezkor - 5 daqiqada kelishi ta'minlanadi. Minimal summa - 500 so'm. \n \n 🛵 Yetkazib berish - Minimal summa - 5000 so'm.  1km - 500 so'm \n \n 🚖 Komfort - Komfort mashina. Konditsioner. Minimal summa - 5000  so'm.  1km - 500 so'm \n \n Barcha tarifda minimal yo'nalish 3 km.",reply_markup=buyurtma)
